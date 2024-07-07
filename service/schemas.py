c271028a1aacf5deca96b18fdd6e3bfaf6cf45b3
PeterKahenya <kahenya0@gmail.com>
2024-07-07 11:59:33 +0300

    add schemas

---
+++ service/schemas.py
@@ -0,0 +1,190 @@
+from pydantic import BaseModel,UUID4
+from typing import List,Optional
+from datetime import datetime
+
+
+class ModelBase(BaseModel):
+    id: UUID4
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class ModelInDBBase(ModelBase):
+    created_at: datetime
+    updated_at: datetime | None
+
+# client app schema
+class ClientAppCreate(BaseModel):
+    name: str
+    description: str
+
+
+class ClientAppUpdate(BaseModel):
+    name: str | None
+    description: str | None
+
+class ClientAppInDBBase(ModelInDBBase):
+    name: str
+    description: str
+    client_id: str
+    client_secret: str
+
+# content type schema
+class ContentTypeCreate(BaseModel):
+    content: str
+
+class ContentTypeUpdate(ContentTypeCreate):
+    pass
+
+class ContentTypeInDBBase(ModelInDBBase):
+    content: str
+
+
+
+# permission schema
+class PermissionCreate(BaseModel):
+    name: str
+    codename: str
+    content_type_id: UUID4
+
+class PermissionUpdate(BaseModel):
+    name: Optional[str] = None
+    codename: Optional[str] = None
+    content_type_id: Optional[UUID4] = None
+
+class PermissionInDBBase(ModelInDBBase):
+    name: str
+    codename: str
+    content_type: ContentTypeInDBBase
+
+
+# role schema
+class RoleCreate(BaseModel):
+    name: str
+    description: str
+
+class RoleUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    permissions: List[ModelBase] | None
+
+class RoleInDBBase(ModelInDBBase):
+    name: str
+    description: str
+    permissions: List[PermissionInDBBase] | None
+
+
+# user schema
+class UserCreate(BaseModel):
+    name: str
+    phone: str
+
+class UserContactCreate(BaseModel):
+    name: str
+    phone: str
+
+class UserContact(BaseModel):
+    name: str
+    phone: str
+    is_active: bool
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    phone: Optional[str] = None
+    phone_verification_code: Optional[str] = None
+    phone_verification_code_expiry_at: Optional[datetime] = None
+    is_active: Optional[bool] = None
+    is_superuser: Optional[bool] = None
+    is_verified: Optional[bool] = None
+    last_seen: Optional[datetime] = None
+    contacts: List[UserContact] | None
+    roles: List[ModelBase] | None
+
+class UserInDBBase(ModelInDBBase):
+    name: str
+    phone: str
+    phone_verification_code_expiry_at: datetime | None
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
+    last_seen: datetime | None
+    contacts: List[UserContact] | None
+    roles: List[RoleInDBBase] | None
+
+# chatroom schema
+class ChatRoomCreate(BaseModel):
+    fcm_room_id: str
+    socket_room_id: str
+    members: Optional[List[ModelBase]] = None
+
+class ChatRoomUpdate(BaseModel):
+    fcm_room_id: Optional[str] = None
+    socket_room_id: Optional[str] = None
+    members: List[ModelBase] | None
+
+# media schema
+class MediaCreate(BaseModel):
+    link: str
+    file_type: str
+
+class MediaUpdate(BaseModel):
+    link: Optional[str] = None
+    file_type: Optional[str] = None
+
+class MediaInDBBase(ModelInDBBase):
+    link: str
+    file_type: str
+
+# chat schema
+class ChatCreate(BaseModel):
+    message: str
+    media: Optional[List[ModelBase]] = None
+    sender: ModelBase | None
+    room: ModelBase
+
+class ChatUpdate(BaseModel):
+    message: Optional[str] = None
+    media: List[ModelBase] | None
+    is_read: Optional[bool] = None
+
+class ChatInDBBase(ModelInDBBase):
+    message: str
+    is_read: bool
+    sender: UserInDBBase
+    room: ModelBase
+    media: List[MediaInDBBase] | None
+
+# chatroomindb schema
+class ChatRoomInDBBase(ModelInDBBase):
+    fcm_room_id: str
+    socket_room_id: str
+    members: List[UserInDBBase] | None
+    room_chats: List[ChatInDBBase] | None
+
+
+# group schema
+class GroupCreate(BaseModel):
+    name: str
+    description: str
+    chatroom: ModelBase
+
+class GroupUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    chatroom: Optional[ModelBase] = None
+
+class GroupInDBBase(ModelInDBBase):
+    name: str
+    description: str
+    chatroom: ChatRoomInDBBase
+
+
+
+
+
+
+
+
+
+
--- service/test_service.py
+++ service/test_service.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine,text
 from sqlalchemy.orm import sessionmaker
 from models import *
 from config import settings,TEST_DATABASE_URL
+from schemas import *
 
 
 @pytest.fixture(scope="session")
@@ -23,6 +24,8 @@ def db():
     Model.metadata.drop_all(bind=engine)
     engine.dispose()
 
+
+"""Test Models"""
 
 #test user creation
 def test_user_creation(db):
@@ -105,7 +108,7 @@ def test_add_media_to_chat(db):
     user2 = User(name="Test User 1", phone="1634567897")
     chatroom = ChatRoom(fcm_room_id="test_fcm_room_id5",socket_room_id="test_socket_room_id5",members=[user1,user2])
     chat = Chat(message="Test Message",sender=user1,room=chatroom)
-    media = Media(link="test_link",type="test_type",chat=chat)
+    media = Media(link="test_link",file_type="test_type",chat=chat)
     db.add(media)
     db.commit()
     assert media in db
@@ -163,5 +166,191 @@ def test_user_role_assignment(db):
     db.add(user)
     db.commit()
     assert role in user.roles
+
+
+
+
+ 
+"""Test Schemas"""
+
+#test clientapp schemas
+def test_clientapp_schema(db):
+    #test create clientapp object
+    client_app = ClientAppCreate(**{"name": "Test App","description": "Test App Description"})
+    assert client_app.name == "Test App"
+    
+    #test update clientapp object
+    client_app_update = ClientAppUpdate(**{"name": "Test App1","description": "Test App Description1"})
+    assert client_app_update.name == "Test App1"
+
+    #test get clientapp object
+    client_app_get = db.query(ClientApp).first()
+    client_app_schema = ClientAppInDBBase.model_validate(client_app_get)
+    assert client_app_schema.name == client_app_get.name
+    assert client_app_schema.description == client_app_get.description
+
+
+#test contenttype schemas
+def test_contenttype_schema(db):
+    #test create contenttype object
+    content_type = ContentTypeCreate(**{"content": "users"})
+    assert content_type.content == "users"
+    
+    #test update contenttype object
+    content_type_update = ContentTypeUpdate(**{"content": "users1"})
+    assert content_type_update.content == "users1"
+
+    #test get contenttype object
+    content_type_get = db.query(ContentType).first()
+    content_type_schema = ContentTypeInDBBase.model_validate(content_type_get)
+    assert content_type_schema.content == content_type_get.content
+
+
+#test permission schemas
+def test_permission_schema(db):
+    #test create permission object
+    content_type = db.query(ContentType).first()
+    permission = PermissionCreate(**{"name": "Can View Users","codename": "view_users","content_type_id": content_type.id})
+    assert permission.name == "Can View Users"
+    assert permission.codename == "view_users"
+    
+    #test update permission object
+    permission_update = PermissionUpdate(**{"name": "Can View Users1","codename": "view_users1","content_type_id": content_type.id})
+    assert permission_update.name == "Can View Users1"
+    assert permission_update.codename == "view_users1"
+
+    #test get permission object
+    permission_get = db.query(Permission).first()
+    permission_schema = PermissionInDBBase.model_validate(permission_get)
+    assert permission_schema.name == permission_get.name
+    assert permission_schema.codename == permission_get.codename
+    assert permission_schema.content_type.content == permission_get.content_type.content
+
+
+#test role schemas
+def test_role_schema(db):
+    #test create role object
+    role_create = RoleCreate(**{"name": "Admin","description": "Admin Role"})
+    assert role_create.name == "Admin"
+    assert role_create.description == "Admin Role"
+    
+    #test update role object
+    permission1 = db.query(Permission).first()
+    permission2 = db.query(Permission).all()[1]
+    role_update = RoleUpdate(**{
+        "name": "Admin1",
+        "description": "Admin Role1",
+        "permissions": [permission1.to_dict(),permission2.to_dict()]
+        })
+    assert role_update.name == "Admin1"
+    assert role_update.description == "Admin Role1"
+
+    #test get role object
+    role_get = db.query(Role).first()
+    role_schema = RoleInDBBase.model_validate(role_get)
+    print(role_schema)
+    assert role_schema.name == role_get.name
+    assert role_schema.description == role_get.description
+    assert role_schema.permissions[0].name == role_get.permissions[0].name
+    assert role_schema.permissions[1].name == role_get.permissions[1].name
+
+
+#test user schemas
+def test_user_schema(db):
+    #test create user object
+    user_create = UserCreate(**{"name": "Test User","phone": "1234567890"})
+    assert user_create.name == "Test User"
+    assert user_create.phone == "1234567890"
+    
+    #test update user object
+    user_update = UserUpdate(**{"name": "Test User1","phone": "1234567891","roles": [],"contacts": []})
+    assert user_update.name == "Test User1"
+    assert user_update.phone == "1234567891"
+
+    #test get user object
+    user_get = db.query(User).first()
+    user_schema = UserInDBBase.model_validate(user_get)
+    assert user_schema.name == user_get.name
+    assert user_schema.phone == user_get.phone
+
+
+#test chatroom schemas
+def test_chatroom_schema(db):
+    #test create chatroom object
+    user1 = db.query(User).first()
+    user2 = db.query(User).all()[1]
+    chatroom_create = ChatRoomCreate(**{"fcm_room_id": "test_fcm_room_id","socket_room_id": "test_socket_room_id","members": [user1.to_dict(),user2.to_dict()]})
+    assert chatroom_create.fcm_room_id == "test_fcm_room_id"
+    assert chatroom_create.socket_room_id == "test_socket_room_id"
+    
+    #test update chatroom object
+    user = db.query(User).all()[2]
+    chatroom_update = ChatRoomUpdate(**{"fcm_room_id": "test_fcm_room_id1","socket_room_id": "test_socket_room_id1","members": [user.to_dict()]})
+    assert chatroom_update.fcm_room_id == "test_fcm_room_id1"
+    assert chatroom_update.socket_room_id == "test_socket_room_id1"
+
+    #test get chatroom object
+    chatroom_get = db.query(ChatRoom).first()
+    chatroom_schema = ChatRoomInDBBase.model_validate(chatroom_get)
+    assert chatroom_schema.fcm_room_id == chatroom_get.fcm_room_id
+    assert chatroom_schema.socket_room_id == chatroom_get.socket_room_id
+
+#test group schemas
+def test_group_schema(db):
+    #test create group object
+    chatroom = db.query(ChatRoom).first()
+    group_create = GroupCreate(**{"name": "Test Group","description": "Test Group Description","chatroom": chatroom.to_dict()})
+    assert group_create.name == "Test Group"
+    assert group_create.description == "Test Group Description"
+    
+    #test update group object
+    user = db.query(User).all()[2]
+    group_update = GroupUpdate(**{"name": "Test Group1","description": "Test Group Description1","chatroom_id": chatroom.id})
+    assert group_update.name == "Test Group1"
+    assert group_update.description == "Test Group Description1"
+
+    #test get group object
+    group_get = db.query(Group).first()
+    group_schema = GroupInDBBase.model_validate(group_get)
+    assert group_schema.name == group_get.name
+    assert group_schema.description == group_get.description
+
+
+#test chat schemas
+def test_chat_schema(db):
+    #test create chat object
+    user1 = db.query(User).first()
+    chatroom = db.query(ChatRoom).first()
+    chat_create = ChatCreate(**{"message": "Test Message","sender": user1.to_dict(),"room": chatroom.to_dict()})
+    assert chat_create.message == "Test Message"
+    
+    #test update chat object
+    media = db.query(Media).first()
+    chat_update = ChatUpdate(**{"message": "Test Message1","is_read": True,"media": [media.to_dict()]})
+    assert chat_update.message == "Test Message1"
+
+    #test get chat object
+    chat_get = db.query(Chat).first()
+    chat_schema = ChatInDBBase.model_validate(chat_get)
+    assert chat_schema.message == chat_get.message
+
+
+#test media schemas
+def test_media_schema(db):
+    #test create media object
+    media_create = MediaCreate(**{"link": "https://test_link","file_type": "mp4"})
+    assert media_create.link == "https://test_link"
+    assert media_create.file_type == "mp4"
+    
+    #test update media object
+    media_update = MediaUpdate(**{"link": "test_link1","file_type": "test_type1"})
+    assert media_update.link == "test_link1"
+    assert media_update.file_type == "test_type1"
+
+    #test get media object
+    media_get = db.query(Media).first()
+    media_schema = MediaInDBBase.model_validate(media_get)
+    assert media_schema.link == media_get.link
+    assert media_schema.file_type == media_get.file_type
 
 
